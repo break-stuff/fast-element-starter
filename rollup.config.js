22f23c21c2d99a047296a4a8a23079bdd8ca3b2e
Burton Smith <bsmith@amazazing.com>
2022-01-25 08:15:44 -0500

    initial commit

---
+++ rollup.config.js
@@ -0,0 +1,10 @@
+import typescript from 'rollup-plugin-typescript2';
+
+export default {
+  input: 'src/index.ts',
+  output: {
+    dir: 'dist',
+    format: 'esm',
+  },
+  plugins: [typescript()],
+};
---
+++ src/my-counter/my-counter.tests.ts
@@ -0,0 +1,33 @@
+import { fixture, assert } from '@open-wc/testing'
+
+// if this gets changed to `import "../src/my-counter` everything works.
+import { MyCounter } from './index'
+
+const tag = new MyCounter().tagName
+
+describe('MyCounter', () => {
+  it('increases the count on button click', async () => {
+    const el = await fixture<MyCounter>(`<${tag}></${tag}>`)
+    const button = el.shadowRoot?.querySelector('button[title="increment"]') as HTMLButtonElement
+    button.click()
+    assert.equal(el.count, 1)
+    await assert.isAccessible(el)
+  })
+
+  it('decreases the count on button click', async () => {
+    const el = await fixture<MyCounter>(`<${tag}></${tag}>`)
+    const button = el.shadowRoot?.querySelector('button[title="decrement"]') as HTMLButtonElement
+    button.click()
+
+    assert.equal(el.count, -1)
+  })
+
+  it('resets to 0', async () => {
+    const el = await fixture<MyCounter>(`<${tag} count="5"></${tag}>`)
+    assert.equal(el.count, 5)
+
+    const button = el.shadowRoot?.querySelector('button[title="reset"]') as HTMLButtonElement
+    button.click()
+    assert.equal(el.count, 0)
+  })
+})
