0b89f4f0b5a1d10cfe6733eafd35717f742838e8
Burton Smith <burton@breakstuff.io>
2022-01-26 12:53:35 -0500

    replace rollup with vite

---
+++ vite.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig, optimizeDeps } from 'vite'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  build: {
+    lib: {
+      entry: 'src/index.ts',
+      name: 'MyLib',
+      fileName: (format) => `index.${format}.js`
+    }
+  },
+  optimizeDeps: {
+      include: ['@open-wc/testing']
+  }
+})
