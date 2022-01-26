38002f35d124a65de85889436447ec06eaf1bd96
Burton Smith <burton@breakstuff.io>
2022-01-26 12:53:58 -0500

    add tests

---
+++ src/my-counter/tests.ts
@@ -0,0 +1,44 @@
+import { fixture, assert } from '@open-wc/testing';
+import { MyCounter } from './index';
+
+const tag = new MyCounter().tagName;
+
+describe('MyCounter', () => {
+  it('increases the count on button click', async () => {
+    // Arrange
+    const el = await fixture<MyCounter>(`<${tag}></${tag}>`);
+    const button = el.shadowRoot?.querySelector('.control-increase') as HTMLButtonElement;
+
+    // Act
+    button.click();
+
+    // Assert
+    assert.equal(el.value, 1);
+    await assert.isAccessible(el);
+  });
+
+  it('decreases the count on button click', async () => {
+    // Arrange
+    const el = await fixture<MyCounter>(`<${tag}></${tag}>`);
+    const button = el.shadowRoot?.querySelector('.control-decrease') as HTMLButtonElement;
+
+    // Act
+    button.click();
+
+    // Assert
+    assert.equal(el.value, -1);
+  });
+
+  it('resets to 0', async () => {
+    // Arrange
+    const el = await fixture<MyCounter>(`<${tag} value="5"></${tag}>`);
+    const button = el.shadowRoot?.querySelector('.reset') as HTMLButtonElement;
+    assert.equal(el.value, 5);
+
+    // Act
+    button.click();
+    
+    // Assert
+    assert.equal(el.value, 0);
+  });
+});
