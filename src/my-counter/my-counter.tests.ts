f54dc49cedde3d6d6ef143a71658eee7919afabb
Burton Smith <burton@breakstuff.io>
2022-02-05 21:30:39 -0500

    remove deprecated test

--- src/my-counter/my-counter.tests.ts
+++ src/my-counter/my-counter.tests.ts
@@ -28,17 +28,4 @@ describe('MyCounter', () => {
     // Assert
     assert.equal(el.value, -1);
   });
-
-  it('resets to 0', async () => {
-    // Arrange
-    const el = await fixture<MyCounter>(`<${tag} value="5"></${tag}>`);
-    const button = el.shadowRoot?.querySelector('.reset') as HTMLButtonElement;
-    assert.equal(el.value, 5);
-
-    // Act
-    button.click();
-    
-    // Assert
-    assert.equal(el.value, 0);
-  });
 });
